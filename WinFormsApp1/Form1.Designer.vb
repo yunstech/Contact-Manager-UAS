5f733e706804db312d621cd051473d47f592e695
Yunus <semiconai@gmail.com>
2024-02-17 15:49:23 +0700

    Add project files.

---
+++ WinFormsApp1/Form1.Designer.vb
@@ -0,0 +1,114 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btn_display = New Button()
+        btn_add = New Button()
+        btn_remove = New Button()
+        ListBox1 = New ListBox()
+        txtFilePath = New TextBox()
+        btn_selectFile = New Button()
+        OpenFileDialog1 = New OpenFileDialog()
+        SuspendLayout()
+        ' 
+        ' btn_display
+        ' 
+        btn_display.Location = New Point(35, 66)
+        btn_display.Name = "btn_display"
+        btn_display.Size = New Size(150, 46)
+        btn_display.TabIndex = 0
+        btn_display.Text = "Display"
+        btn_display.UseVisualStyleBackColor = True
+        ' 
+        ' btn_add
+        ' 
+        btn_add.Location = New Point(222, 66)
+        btn_add.Name = "btn_add"
+        btn_add.Size = New Size(150, 46)
+        btn_add.TabIndex = 1
+        btn_add.Text = "Add"
+        btn_add.UseVisualStyleBackColor = True
+        ' 
+        ' btn_remove
+        ' 
+        btn_remove.Location = New Point(415, 66)
+        btn_remove.Name = "btn_remove"
+        btn_remove.Size = New Size(150, 46)
+        btn_remove.TabIndex = 2
+        btn_remove.Text = "Remove"
+        btn_remove.UseVisualStyleBackColor = True
+        ' 
+        ' ListBox1
+        ' 
+        ListBox1.FormattingEnabled = True
+        ListBox1.Location = New Point(35, 244)
+        ListBox1.Name = "ListBox1"
+        ListBox1.Size = New Size(1588, 676)
+        ListBox1.TabIndex = 3
+        ' 
+        ' txtFilePath
+        ' 
+        txtFilePath.Location = New Point(998, 73)
+        txtFilePath.Name = "txtFilePath"
+        txtFilePath.Size = New Size(439, 39)
+        txtFilePath.TabIndex = 5
+        ' 
+        ' btn_selectFile
+        ' 
+        btn_selectFile.Location = New Point(1462, 66)
+        btn_selectFile.Name = "btn_selectFile"
+        btn_selectFile.Size = New Size(150, 46)
+        btn_selectFile.TabIndex = 4
+        btn_selectFile.Text = "Select File"
+        btn_selectFile.UseVisualStyleBackColor = True
+        ' 
+        ' OpenFileDialog1
+        ' 
+        OpenFileDialog1.FileName = "openFileDialog1"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(13.0F, 32.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1655, 1057)
+        Controls.Add(txtFilePath)
+        Controls.Add(btn_selectFile)
+        Controls.Add(ListBox1)
+        Controls.Add(btn_remove)
+        Controls.Add(btn_add)
+        Controls.Add(btn_display)
+        Name = "Form1"
+        Text = "Contact Manager"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btn_display As Button
+    Friend WithEvents btn_add As Button
+    Friend WithEvents btn_remove As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents txtFilePath As TextBox
+    Friend WithEvents btn_selectFile As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+
+End Class
---
+++ WinFormsApp1/Form1.vb
@@ -0,0 +1,104 @@
+﻿Imports System.IO
+
+Public Class Form1
+
+    Private contacts As New List(Of String)
+    Private filePath As String = ""
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' No need to load contacts here
+    End Sub
+
+    Private Sub LoadContactsFromFile(filePath As String)
+        If File.Exists(filePath) Then
+            contacts.Clear() ' Clear existing contacts before loading from file
+            Using reader As New StreamReader(filePath)
+                Dim line As String
+                Do
+                    line = reader.ReadLine()
+                    If line IsNot Nothing Then
+                        contacts.Add(line)
+                    End If
+                Loop Until line Is Nothing
+            End Using
+            DisplayContacts() ' Automatically display contacts after loading from file
+        End If
+    End Sub
+
+    Private Sub DisplayContacts()
+        ListBox1.Items.Clear() ' Clear ListBox before displaying contacts
+        If contacts.Count > 0 Then
+            For Each contact As String In contacts
+                ListBox1.Items.Add(contact)
+            Next
+        Else
+            ListBox1.Items.Add("No contacts available.")
+        End If
+    End Sub
+
+    Private Sub SaveContactsToFile(filePath As String)
+        Using writer As New StreamWriter(filePath)
+            For Each contact As String In contacts
+                writer.WriteLine(contact)
+            Next
+        End Using
+    End Sub
+
+    Private Sub AddContact(contactName As String, phoneNumber As String)
+        contacts.Add($"{contactName} - {phoneNumber}")
+        DisplayContacts() ' Automatically display contacts after adding a contact
+    End Sub
+
+    Private Sub RemoveContact(contactName As String)
+        ' Search for the contact with the given name
+        For Each contact As String In contacts
+            If contact.StartsWith(contactName & " - ") Then
+                contacts.Remove(contact)
+                Exit For ' Exit loop after removing the contact
+            End If
+        Next
+        DisplayContacts() ' Automatically display contacts after removing a contact
+    End Sub
+
+    Private Sub btn_display_Click(sender As Object, e As EventArgs) Handles btn_display.Click
+        DisplayContacts()
+    End Sub
+
+    Private Sub btn_add_Click(sender As Object, e As EventArgs) Handles btn_add.Click
+        Dim newContactName As String = InputBox("Enter the name of the new contact:")
+        Dim newContactNumber As String = InputBox("Enter the phone number of the new contact:")
+
+        If Not String.IsNullOrEmpty(newContactName) AndAlso Not String.IsNullOrEmpty(newContactNumber) Then
+            AddContact(newContactName, newContactNumber)
+        End If
+    End Sub
+
+    Private Sub btn_remove_Click(sender As Object, e As EventArgs) Handles btn_remove.Click
+        Dim contactToRemove As String = InputBox("Enter the name of the contact to remove:")
+        If Not String.IsNullOrEmpty(contactToRemove) Then
+            RemoveContact(contactToRemove)
+        End If
+    End Sub
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        If Not String.IsNullOrEmpty(filePath) Then
+            SaveContactsToFile(filePath)
+        End If
+    End Sub
+
+    Private Sub btn_selectFile_Click(sender As Object, e As EventArgs) Handles btn_selectFile.Click
+        Dim openFileDialog1 As New OpenFileDialog
+
+        openFileDialog1.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*"
+        openFileDialog1.FilterIndex = 1
+        openFileDialog1.RestoreDirectory = True
+
+        If openFileDialog1.ShowDialog = DialogResult.OK Then
+            ' Display the selected file path
+            filePath = openFileDialog1.FileName
+            txtFilePath.Text = filePath
+            ' Load contacts from the selected file
+            LoadContactsFromFile(filePath)
+        End If
+    End Sub
+End Class
